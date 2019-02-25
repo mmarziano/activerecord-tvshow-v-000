29fc65d77b5cc09807fcf3ab59277a5f0047f6c6
Monica Marziano <mmarziano1@gmail.com>
2019-02-25 23:31:56 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base 
   
+  def self.highest_rating
+    Show.maximum('rating')
+  end 
+  
+  def self.highest_rating
+    Show.find_by(self.highest_rating)
+  end
 end 
